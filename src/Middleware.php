a25fc9cc146192e1cedd9fefc3c8fe81c4437479
Jeremy Walker <jeremy@webconcepts.co.nz>
2022-12-21 17:16:02 +1300

    Set xsrf-token cookie so that Axios will automatically return the security token with each request

--- src/Middleware.php
+++ src/Middleware.php
@@ -3,13 +3,26 @@
 namespace Inertia;
 
 use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Control\Cookie;
+use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Security\SecurityToken;
 
 class Middleware implements HTTPMiddleware
 {
     public function process(HTTPRequest $request, callable $delegate)
     {
+        $securityToken = SecurityToken::inst();
+
+        if ($securityToken->isEnabled() && $request->getHeader('X-XSRF-TOKEN')) {
+            $this->addXsrfTokenFromAxiosToRequest($request);
+        }
+
         $response = $delegate($request);
+
+        if ($securityToken->isEnabled() && !headers_sent() && !Director::is_cli()) {
+            $this->setXsrfTokenCookieForAxios();
+        }
 
         // If we have no X-Inertia header continue
         if (!$request->getHeader('X-Inertia')) {
@@ -21,5 +34,30 @@ class Middleware implements HTTPMiddleware
 
         // Don't forget to the return the response!
         return $response;
+    }
+
+    /**
+     * Copy the security token that Axois provides in the X-XSRF-TOKEN header,
+     * to the X-SecurityID header that SilverStripe's security token check
+     * will look for.
+     */
+    protected function addXsrfTokenFromAxiosToRequest(HTTPRequest $request)
+    {
+        $securityTokenName = SecurityToken::inst()->getName();
+
+        $request->addHeader("X-{$securityTokenName}", $request->getHeader('X-XSRF-TOKEN'));
+    }
+
+    /**
+     * Provide a XSRF-TOKEN cookie for Axios to have it automatically include
+     * the token with each request it makes, in the X-XSRF-TOKEN header
+     */
+    protected function setXsrfTokenCookieForAxios()
+    {
+        Cookie::set(
+            name: 'XSRF-TOKEN',
+            value: SecurityToken::inst()->getValue(),
+            httpOnly: false
+        );
     }
 }
